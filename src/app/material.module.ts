636810bba2776424a3fc8754a1d4e3ba82ab466d
Tucker <tuckeralan.dev@gmail.com>
2020-01-20 16:39:59 -0600

    Modify Request component to utilize Angular Material markup

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -1,10 +1,15 @@
 import { NgModule } from '@angular/core';
 
-import { MatButtonModule } from '@angular/material';
+import { MatNativeDateModule, MatIconModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
+import { MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatListModule } from '@angular/material';
+import {MatDatepickerModule} from '@angular/material/datepicker';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
-    imports: [MatButtonModule],
-    exports: [MatButtonModule]
-})
+    imports: [MatNativeDateModule, MatDatepickerModule, MatIconModule, MatButtonModule, MatCheckboxModule,
+        MatToolbarModule, FormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatRadioModule],
+    exports: [MatNativeDateModule, FormsModule, MatDatepickerModule, MatIconModule, MatButtonModule, MatCheckboxModule,
+        MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatRadioModule]
+    })
 
 export class MaterialModule { }
