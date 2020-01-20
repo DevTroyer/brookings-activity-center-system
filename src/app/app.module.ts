668160caff9571627be2aa00e2640257ba9d6f22
Tucker <tuckeralan.dev@gmail.com>
2020-01-20 16:09:46 -0600

    Create Material module to organize imported Material components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,6 +4,7 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MaterialModule } from './material.module';
 import { LoginComponent } from './login/login.component';
 import { RequestComponent } from './request/request.component';
 
@@ -16,7 +17,8 @@ import { RequestComponent } from './request/request.component';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    MaterialModule
   ],
   providers: [],
   bootstrap: [AppComponent]
