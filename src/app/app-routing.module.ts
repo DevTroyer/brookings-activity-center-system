393c9dd63b0336f1088b962c98e5ac37e1d79acf
Tucker <tuckeralan.dev@gmail.com>
2020-01-20 14:01:10 -0600

    Create login and request components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,15 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RequestComponent } from './request/request.component';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RequestComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'request', component: RequestComponent },
+  { path: '**', redirectTo: '' }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,14 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { LoginComponent } from './login/login.component';
+import { RequestComponent } from './request/request.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RequestComponent
   ],
   imports: [
     BrowserModule,
