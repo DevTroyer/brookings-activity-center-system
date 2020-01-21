56a42ac46adf311904b2ef389466299768556d0d
Tucker <tuckeralan.dev@gmail.com>
2020-01-20 19:52:39 -0600

    Add fields to select event dates on Request component

--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -2,14 +2,17 @@ import { NgModule } from '@angular/core';
 
 import { MatNativeDateModule, MatIconModule, MatButtonModule, MatCheckboxModule } from '@angular/material';
 import { MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatRadioModule, MatListModule } from '@angular/material';
+import { SatDatepickerModule, SatNativeDateModule } from 'saturn-datepicker';
 import {MatDatepickerModule} from '@angular/material/datepicker';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
     imports: [MatNativeDateModule, MatDatepickerModule, MatIconModule, MatButtonModule, MatCheckboxModule,
-        MatToolbarModule, FormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatRadioModule],
+        MatToolbarModule, FormsModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatRadioModule,
+        SatDatepickerModule, SatNativeDateModule],
     exports: [MatNativeDateModule, FormsModule, MatDatepickerModule, MatIconModule, MatButtonModule, MatCheckboxModule,
-        MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatRadioModule]
+        MatToolbarModule, MatCardModule, MatFormFieldModule, MatInputModule, MatListModule, MatRadioModule,
+        SatDatepickerModule, SatNativeDateModule]
     })
 
 export class MaterialModule { }
