a585fd1e564d70c1a3fd08117b5969d014b018e7
Tucker <tuckeralan.dev@gmail.com>
2020-02-07 16:14:38 -0600

    Add .gitignore and import new web API

--- BrookingsActivityCenterSystem/Program.cs
+++ BrookingsActivityCenterSystem/Program.cs
@@ -7,7 +7,7 @@ using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 
-namespace Server
+namespace BrookingsActivityCenterSystem
 {
     public class Program
     {
